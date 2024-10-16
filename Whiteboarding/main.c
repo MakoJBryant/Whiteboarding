ca2d44ab1f6d736d68a875b25262fcb25b841d67
Mako Joseph Bryant <makojbryant@gmail.com>
2024-10-15 17:13:58 -0700

    Copy array a into array b, using a loop.

--- Whiteboarding/main.c
+++ Whiteboarding/main.c
@@ -3,27 +3,18 @@
 #include <stdio.h>
 
 
-// Fill in the code to copy the content of array `a` into array `b` not using a loop:
+// Fill in the code to copy the content of array `a` into array `b` using a loop:
 int main(void) {
 
     int a[5] = { 1, 2, 3, 4, 5 };
     int b[5] = { 0 };
 
     //your code here
-    b[0] = a[0];
-    printf("%d", b[0]);
-
-    b[1] = a[1];
-    printf("%d", b[1]);
-    
-    b[2] = a[2];
-    printf("%d", b[2]);
-    
-    b[3] = a[3];
-    printf("%d", b[3]);
-    
-    b[4] = a[4];
-    printf("%d", b[4]);
+    for (int i = 0; i < 5; i++)
+    {
+        b[i] = a[i];
+        printf("%d", b[i]);
+    }
 
     return 0;
 
