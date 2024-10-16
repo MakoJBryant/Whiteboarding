4bad611be76293213e346214aa9910ba31557258
Mako Joseph Bryant <makojbryant@gmail.com>
2024-10-15 18:13:14 -0700

    Wrote a function that prints an array.

--- Whiteboarding/main.c
+++ Whiteboarding/main.c
@@ -3,18 +3,34 @@
 #include <stdio.h>
 
 
-// Fill in the code to copy the content of array `a` into array `b` using a loop:
+/*
+Write a function that does the following:
+Name: 		print_array
+Parameters: int, size: the size of the arrays to be printed
+            int array, input: the array continaing the data to be printed
+Return: 	none
+Note:		This function should print the content of the array to the console
+*/
+
+void print_array(int size, int array[]) {
+
+    for (int i = 0; i < size; i++)
+    {
+        int temp = array[i];
+        printf("%d", temp);
+    }
+
+    return;
+}
+
 int main(void) {
 
     int a[5] = { 1, 2, 3, 4, 5 };
-    int b[5] = { 0 };
 
-    //your code here
-    for (int i = 0; i < 5; i++)
-    {
-        b[i] = a[i];
-        printf("%d", b[i]);
-    }
+    print_array(5, a);
+
+    //printed to console:
+    //1 2 3 4 5 
 
     return 0;
 
