9b37f81d6028a6ab2cdfd366bad11fa3e5b57bb8
Mako J. Bryant <mako.j.bryant@outlook.com>
2024-10-24 10:49:26 -0700

    Created sort, sorted, and insert_sorted functions without using [] operator.

--- Whiteboarding/main.c
+++ Whiteboarding/main.c
@@ -1,5 +1,79 @@
 #include <stdio.h>
 
+void sort(int size, int* a);
+int sorted(int size, int* a);
+void insert_sorted(int size, int* a, int* b);
+void reverse(int size, int* a);
+void copy(int size, int* input, int* output);
+void print(int size, int* a);
+
+// Main function.
+int main(void) {
+
+	int a[10] = { -4, 1, 7, 2, 5, 6, 3, -8, 9, -2 };
+	int b[10] = { 0 };
+
+	insert_sorted(10, a, b); //implement this function!
+	print(10, b); //implement this function!
+	//prints: -8, -4, -2, 1, 2, 3, 5, 6, 7, 9
+
+	return 0;
+}
+
+// Sort an array from least (at left) to most (at right).
+void sort(int size, int* a) {
+
+	// While array is not sorted:
+	while (!sorted(size, a)) {
+
+		// For each element of the array:
+		for (int i = 0; i < size - 1; i++)
+		{
+
+			// If the current index is greater than the next index:
+			if (*(a + i) > *(a + i + 1)) {
+
+				// Save value of array at current index.
+				int temp = *(a + i);
+
+				// Set value at current index to value at next index.
+				*(a + i) = *(a + i + 1);
+
+				// Set value at next index with original value at current index.
+				*(a + i + 1) = temp;
+			}
+		}
+	}
+}
+
+// Check if a given array is sorted without using [] operator.
+int sorted(int size, int* a) {
+
+	// Loop through array from first element to last:
+	for (int i = 0; i < size; i++) {
+
+		// If current element is less than previous element:
+		if (*(a + i) < *(a + i - 1)) {
+
+			// Array is not sorted.
+			return 0;
+		}
+	}
+
+	// Array is sorted.
+	return 1;
+}
+
+// Sort an array without using the [] operator.
+void insert_sorted(int size, int* a, int* b) {
+
+	// Copy array a into array b.
+	copy(size, a, b);
+	
+	// Sort array b.
+	sort(size, b);
+}
+
 // Reverse an array without using the [] operator.
 void reverse(int size, int* a) {
 
@@ -17,6 +91,14 @@ void reverse(int size, int* a) {
 	}
 }
 
+// Copy one array into another without using the [] operator.
+void copy(int size, int* input, int* output) {
+	for (int i = 0; i < size; i++)
+	{
+		*(output + i) = *(input + i);
+	}
+}
+
 // Print an array without using the [] operator.
 void print(int size, int* a) {
 	for (int i = 0; i < size - 1; i++)
@@ -24,16 +106,4 @@ void print(int size, int* a) {
 		printf("%d, ", *(a + i));
 	}
 	printf("%d", *(a + size - 1));
-}
-
-// Main function.
-int main(void) {
-
-	int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-	reverse(10, a); //implement this function!
-	print(10, a); //implement this function!
-	//prints: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
-
-	return 0;
 }
