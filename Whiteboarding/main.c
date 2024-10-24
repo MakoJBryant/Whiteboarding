a4dc43cfdee6127646a9f921a6925f41e7cf24db
Mako J. Bryant <mako.j.bryant@outlook.com>
2024-10-24 09:54:55 -0700

    Created reverse array function without using the [] operator.

--- Whiteboarding/main.c
+++ Whiteboarding/main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
-// Copy one array into another without using the [] operator.
-void copy(int size, int* input, int* output) {
-	for (int i = 0; i < size; i++)
+// Reverse an array without using the [] operator.
+void reverse(int size, int* a) {
+
+	// Loop through half of the array.
+	for (int i = 0; i < (size / 2); i++)
 	{
-		*(output + i) = *(input + i);
+		// Store current element value temporarily.
+		int temp = *(a + i);
+
+		// Set current element as (i) away from last element.
+		*(a + i) = *(a + size - 1 - i);
+
+		// Set (i) away from first element as stored value.
+		*(a + size - 1 - i) = temp;
 	}
 }
 
 // Print an array without using the [] operator.
-void print(int size, int* output) {
+void print(int size, int* a) {
 	for (int i = 0; i < size - 1; i++)
 	{
-		printf("%d, ", *(output + i));
+		printf("%d, ", *(a + i));
 	}
-	printf("%d", *(output + size - 1));
+	printf("%d", *(a + size - 1));
 }
 
 // Main function.
 int main(void) {
 
-	int input[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int output[10] = { 0 };
+	int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	copy(10, input, output); //implement this function!
-	print(10, output); //implement this function!
-	//prints: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+	reverse(10, a); //implement this function!
+	print(10, a); //implement this function!
+	//prints: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 
 	return 0;
-
 }
